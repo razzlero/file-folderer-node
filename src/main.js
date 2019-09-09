1f8f83cb697c855ecead461a581f7458e357cddf
razzlero <8150355+razzlero@users.noreply.github.com>
2019-09-10 00:02:23 +0900

    Take params

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+/* eslint-disable no-console */
 const fs = require('fs');
+const path = require('path');
 
 const testFolder = './';
 
+const fileToFolder = process.argv[2];
+
+if (!fileToFolder) {
+  console.log('Need to pass the file to folder as an argument.');
+  process.exit(1);
+}
+
+const parentDir = path.dirname(fileToFolder);
+
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
-      console.log(file);
+    console.log(file);
   });
 });
 
