ac7808e39b5e2d574a4f0f3aae0433e3f7d10fb8
razzlero <8150355+razzlero@users.noreply.github.com>
2019-09-10 10:22:26 +0900

    Add commander

--- src/main.js
+++ src/main.js
@@ -1,18 +1,46 @@
 /* eslint-disable no-console */
+const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 
-const testFolder = './';
+program
+  .version('0.0.1')
+  .description('File Folderer')
+  .option('-f, --file <fileName>', 'File to process')
+  .option('-d, --directory <directoryPath>', 'Directory to process');
 
-const fileToFolder = process.argv[2];
+// Very useful looking article, includes commander
+// https://blog.developer.atlassian.com/scripting-with-node/
 
-if (!fileToFolder) {
-  console.log('Need to pass the file to folder as an argument.');
-  process.exit(1);
+// TODO: take a directory by default. take an optional file name/list for specific files to process
+// if no files are specified processes all files in the folder.
+
+// Maybe if no directory is specified do the current directory?
+// Add  dry run option
+// Maybe stop for confirmaiton by default unless a -y flag is given (coudl ue inquirer for asking)
+
+//program
+//  .command('file <fileToFolder>')
+//  .alias('f')
+//  .description('Move a file into a subfolder with the closest matching name.')
+//  .action((fileToFolder) => {  
+//	const parentDir = path.dirname(fileToFolder);
+//	console.log(parentDir);
+//    //TODO
+//	console.log("TODO");
+//  });
+
+program.parse(process.argv);
+
+console.log(program.opts())
+
+if (program.file) {
+  console.log('Process single file');
+  console.log("TODO");
 }
 
-const parentDir = path.dirname(fileToFolder);
 
+const testFolder = './';
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
     console.log(file);
