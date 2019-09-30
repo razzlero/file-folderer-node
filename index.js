2dd668557688fff1729e23acba4317da476bd3d6
razzlero <8150355+razzlero@users.noreply.github.com>
2019-09-30 18:15:23 +0900

    Some basic commander code

--- index.js
+++ index.js
@@ -2,13 +2,35 @@
 
 const program = require('commander');
 const fs = require('fs');
-const path = require('path');
 
 program
   .version('0.0.1')
   .description('File Folderer')
-  .option('-f, --file <fileName>', 'File to process')
-  .option('-d, --directory <directoryPath>', 'Directory to process');
+  .arguments('[files...]')
+  .option('-d, --directory <directoryPath>', 'Directory to process')
+  .action((files) => {
+    const processDir = program.directory || './';
+    console.log('dir:');
+    console.log(processDir);
+
+    if (files) {
+      console.log('argument files');
+      console.log(files);
+    }
+
+    fs.readdir(processDir, (err, dirFiles) => {
+      console.log('dir files');
+      dirFiles.forEach((file) => {
+        console.log(file);
+      });
+    });
+
+    if (program.file) {
+      console.log('Process single file');
+      console.log(program.file);
+    }
+  })
+  .parse(process.argv);
 
 // Very useful looking article, includes commander
 // https://blog.developer.atlassian.com/scripting-with-node/
@@ -18,34 +40,4 @@ program
 
 // Maybe if no directory is specified do the current directory?
 // Add  dry run option
-// Maybe stop for confirmaiton by default unless a -y flag is given (coudl ue inquirer for asking)
-
-//program
-//  .command('file <fileToFolder>')
-//  .alias('f')
-//  .description('Move a file into a subfolder with the closest matching name.')
-//  .action((fileToFolder) => {
-//  const parentDir = path.dirname(fileToFolder);
-//  console.log(parentDir);
-//    //TODO
-//  console.log("TODO");
-//  });
-
-program.parse(process.argv);
-
-console.log(program.opts())
-
-if (program.file) {
-  console.log('Process single file');
-  console.log('TODO');
-}
-
-
-const testFolder = './';
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
-
-console.log('testing');
+// Maybe stop for confirmation by default unless a -y flag is given (could use inquirer for asking)
