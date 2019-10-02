fa0636d32841737e1d09b629e74cd0a18cf47520
razzlero <8150355+razzlero@users.noreply.github.com>
2019-10-02 18:23:05 +0900

    Added mock-fs

--- index.test.js
+++ index.test.js
@@ -1,3 +1,17 @@
+const mock = require('mock-fs');
+
+// Setup
+// https://www.npmjs.com/package/mock-fs
+mock({
+  'path/to/fake/dir': {
+    'some-file.txt': 'file content here',
+    'empty-dir': {/** empty directory */},
+  },
+  'path/to/some.png': Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+  'some/other/path': {/** another empty directory */},
+});
+
+// Tests
 test('simple pass', () => {
   expect(1).toBe(1);
 });
@@ -5,3 +19,6 @@ test('simple pass', () => {
 test('simple fail', () => {
   expect(1).toBe(2);
 });
+
+// Cleanup
+mock.restore();
